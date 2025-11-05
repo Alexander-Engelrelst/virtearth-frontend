35dbb932c20d0a8c925038ef0a98fce98771ca0d
Kobe Vandenberghe <kobe.vandenberghe@student.howest.be>
2025-11-05 11:42:41 +0100

    implements vue router & tailwind/daisyui config

---
+++ VirtEarth-client/src/router/index.js
@@ -0,0 +1,8 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+export default createRouter({
+  history: createWebHistory(),               // use createWebHashHistory() if you prefer # URLs
+  routes: [
+    { path: '/', name: 'logon', component: () => import('@/pages/Logon.vue') },
+  ],
+})
