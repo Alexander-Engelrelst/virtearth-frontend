afe16901974a08cfdbef06603d8bfaf4b651047d
Kobe Vandenberghe <kobe.vandenberghe@student.howest.be>
2025-11-06 08:46:10 +0100

    adds theme styles and update font configurations

--- VirtEarth-client/src/components/base/BrandTitle.vue
+++ VirtEarth-client/src/components/base/BrandTitle.vue
@@ -8,7 +8,7 @@ defineProps({
 </script>
 
 <template>
-    <h1 class="font-bold text-brand-primary" :style="{ fontSize: size + 'px' }">
+    <h1 class="font-bold text-brand-primary" :style="{ fontSize: size + 'px', fontFamily: 'var(--font-primary)' }">
         Virtearth
     </h1>
 </template>
