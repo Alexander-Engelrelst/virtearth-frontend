b51a66314271fdc03e3eb4c53c75bb79f05aea64
Kobe Vandenberghe <kobe.vandenberghe@student.howest.be>
2025-11-07 10:50:51 +0100

    fixes memory leak in MapContainer

--- VirtEarth-client/src/components/feature/MapContainer.vue
+++ VirtEarth-client/src/components/feature/MapContainer.vue
@@ -1,17 +1,25 @@
 <script setup>
-import { onMounted } from 'vue'
+import { onBeforeUnmount, onMounted, ref } from 'vue'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 
+
+const mapInstance = ref(null)
+
 onMounted(() => {
-    const map = L.map('map').setView([0, 0], 3)
+    mapInstance.value = L.map('map').setView([0, 0], 3)
     var Stadia_StamenWatercolor = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.{ext}', {
         minZoom: 1,
         maxZoom: 16,
         attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         ext: 'jpg'
     }); 
-    Stadia_StamenWatercolor.addTo(map);
+    Stadia_StamenWatercolor.addTo(mapInstance.value);
+})
+onBeforeUnmount(() => {
+    if (mapInstance.value) {
+        mapInstance.value.remove()
+    }
 })
 </script>
 
