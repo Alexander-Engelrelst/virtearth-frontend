847c44d85a39d9ff05dfb3d791a78980a920217c
Kobe Vandenberghe <kobe.vandenberghe@student.howest.be>
2025-12-02 14:40:45 +0100

    changes icon usage based on completed field for landmarks

--- VirtEarth-client/src/components/feature/MapContainer.vue
+++ VirtEarth-client/src/components/feature/MapContainer.vue
@@ -2,7 +2,8 @@
 import { onBeforeUnmount, onMounted, ref, watch } from "vue";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import markerIcon from "@/assets/images/adria_landmark_marker_purple.png";
+import markerIconPurple from "@/assets/images/adria_landmark_marker_purple.png";
+import markerIcon from "@/assets/images/adria_landmark_marker.png";
 
 const props = defineProps({
   landmarks: {
@@ -48,15 +49,16 @@ const updateMarkers = () => {
   }
 
   if (props.landmarks.length > 0) {
-    const icon = L.icon({
-      iconUrl: markerIcon,
-      iconSize: [60, 60],
-      iconAnchor: [30, 60],
-    });
 
-    const markers = props.landmarks.map(({ latitude, longitude, year, gameName }) =>
-      L.marker([latitude, longitude], { icon }).bindPopup(year + " " + gameName, {offset: [0, -40]})
-    );
+    const markers = props.landmarks.map(({ latitude, longitude, year, gameName, completed }) => {
+      const currentIcon = completed ? markerIconPurple : markerIcon;
+      const icon = L.icon({
+        iconUrl: currentIcon,
+        iconSize: [60, 60],
+        iconAnchor: [30, 60],
+      });
+      return L.marker([latitude, longitude], { icon }).bindPopup(year + " " + gameName, {offset: [0, -40]});
+    });
 
     markersGroup.value = L.featureGroup(markers).addTo(mapInstance.value);
   }
