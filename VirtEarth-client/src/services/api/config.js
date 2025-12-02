ef8f103d0705abb6eed8bee8addee6e53a4dddbb
Kobe Vandenberghe <kobe.vandenberghe@student.howest.be>
2025-12-02 10:50:37 +0100

    switches MOCK API const to false

--- VirtEarth-client/src/services/api/config.js
+++ VirtEarth-client/src/services/api/config.js
@@ -1,5 +1,5 @@
 // Set to true to use mock data instead of real API calls
-export const USE_MOCK_API = true;
+export const USE_MOCK_API = false;
 
 // Base URL for API requests
 export const API_BASE_URL = "https://localhost:44301";
